6904aa83f648fc5e6648ff0789a36b657a1b9328
Srinizkumar Konakanchi <srinizkumar.konakanchi@gmail.com>
2016-08-16 00:52:50 -0700

    Update Connection.java

--- src/com/sfdcreporttofileapp/wb/Connection.java
+++ src/com/sfdcreporttofileapp/wb/Connection.java
@@ -12,7 +12,7 @@ package com.sfdcreporttofileapp.wb;
 public interface Connection {
 	/* QA    */
 	String USERNAME = "srinizkumar.konakanchi@wbconsultant.com.qa";
-	String PASSWORD = "Indian06#yMkAb2he7b9Pb30O2O9mgrPgC";
+	String PASSWORD = "";
 	String url = "https://test.salesforce.com/services/oauth2/token";
     String client_id = "3MVG9FS3IyroMOh4JaFY_ohSyLR9_04OwuexOfukzHJnjrqp1QFGJutM_0.cHfpXe3U1Q9Y2DVirpQcqevSHq";
     String client_secret = "4483528363863041495";
